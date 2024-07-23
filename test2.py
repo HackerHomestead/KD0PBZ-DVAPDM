c29a0964bf3bd0537ed8c4f355822d9cb5892d16
Handy Hacker <handy@hackerhomestead.com>
2024-07-23 09:07:05 -0500

    Adding help

--- test2.py
+++ test2.py
@@ -34,37 +34,54 @@ data = "08002002C0B7BB08" # LSB 0x08BBB7C0
 # 0500180000 # DVAP in inactive idle state
 # 0500180001 # DVAP in active state
 
-# Will get this when RX is on (1) when TX is on
-# 0520180100
+CMD_INDEX = {
+        "Request Name":                 "04200100",
+        "Request Status / Error":       "04200500",
+        "Request Status":               "04200500",
+        "Set GMSK Mode" :               "0500280001",
+        "Put into Normal Mode":         "05002a0000",
+        "Put into CW Test Mode":        "05002a0001",
+        "Put into Deviation Test Mode": "05002a0002",
+        "Set Squelch to -100dBm"        "050080009C",
+        "Set RX+TX to 146.520":         "08002002C0B7BB08",
+        "Put DVAP into active state":   "0500180001",
+        "Put DVAP into inactive state": "0500180000",
+        }
 
-bytes_object = bytes.fromhex(data)
-print(data)
-ser.write(bytes_object)
-
+MAX_LINE = 16
+CHAR_COUNT = 16
 # Read and print out any incoming data from the serial port
 print("Listening...")
 while True:
     try:
         rx = True
+        line_width = 16
         while rx:
             incoming_data = ser.read(1)  # Read up to 1 byte with a 100ms timeout
             if incoming_data:
                 print(f"{incoming_data.hex()} ", end='')
                 rx = True
+                CHAR_COUNT = CHAR_COUNT - 1
+                if CHAR_COUNT == 0:
+                    print("")
+                    CHAR_COUNT = MAX_LINE
             else:
                 print("")
                 rx = False
-
-        #Ok next step is to read until the buffer is empty
+                CHAR_COUNT = MAX_LINE
+                
 
         data = input("HEX:")
         if len(data) > 0:
             if data == "quit":
                 print("Exiting ... ")
                 sys.exit(0)
-            bytes_object = bytes.fromhex(data)
-            #print("SND:", data)
-            ser.write(bytes_object)
+            if data == "help":
+                for item in CMD_INDEX:
+                    print(item, ": " , CMD_INDEX[item])
+            else:
+                bytes_object = bytes.fromhex(data)
+                ser.write(bytes_object)
 
         
     except serial.SerialTimeoutException:
