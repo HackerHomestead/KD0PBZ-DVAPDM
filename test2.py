b497a3e46a5ec74c831dc43ca2d2e934ca9a7061
Handy Hacker <handy@hackerhomestead.com>
2024-07-24 17:04:59 -0500

    Cleanup

--- test2.py
+++ test2.py
@@ -5,6 +5,19 @@
 # 
 # Next steps would be to actually init the device with RX/TX frequancies
 #  and tx or rx something.
+#
+# Still have not found an example of a packet, but then again have also not been able
+#  to get it to give me a RX of a voice/data packet.
+#
+# While we work on this we really need to move towards a duplex interface so that
+#  I can intrupt if I am getting a large amount of packets, along those lines also
+#  should figure out a way to emulate a device so that I can work on code when its not
+#  connected or so that I can do other simulations. 
+#
+# Idea would be to look into one of the other stacks that is open source since the packet
+#  should be the same at least the bulk of it.
+#  Further the DVAP tool has recording features, and some canned stuff I should see about
+#  decoding it as an example
 
 import sys
 import serial
@@ -15,24 +28,7 @@ port = '/dev/ttyUSB0'  # Replace with your desired port (e.g. /dev/ttyUSB0, COM1
 baudrate = 230400
 ser = serial.Serial(port, baudrate, timeout=1)
 
-print("Sending ... ")
 # Write the array of hex values to the serial device
-
-# Make it say DVAP Dongle (ASCII String)
-data = "04200100" # [04][20] [01][00]
-
-# Request Status / Error
-data = "04200500" # [04][20] [05][00] 
-
-# Modulation Type
-data = "0500280001" # Set GMSK Mode
-
-# Set RX/TX to 146.520
-data = "08002002C0B7BB08" # LSB 0x08BBB7C0
-
-# Set Run State
-# 0500180000 # DVAP in inactive idle state
-# 0500180001 # DVAP in active state
 
 CMD_INDEX = {
         "Request Name":                 "04200100",
